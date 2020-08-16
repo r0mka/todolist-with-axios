c7e1a7e417c7afb12a2fd93527167b05094926f2
r0mka <romanliveshere@gmail.com>
2020-08-16 00:38:43 -0700

    comment out useEffect

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -47,9 +47,9 @@ export default function TodoList() {
     if (nodeNames.includes(e.target.nodeName)) return true;
   };
 
-  React.useEffect(() => {
-    fetchAndUpdateList();
-  }, []);
+  // React.useEffect(() => {
+  //   fetchAndUpdateList();
+  // }, []);
 
   return (
     <div className="TodoList">
