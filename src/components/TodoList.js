c65bd05b5016b9212703a4644985efbd59270e30
r0mka <romanliveshere@gmail.com>
2020-08-16 15:55:55 -0700

    Fix useEffect dependency

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoList.css';
-import { initialList } from '../data/initialList';
-import { useTodos } from '../customHooks/useTodos';
+import arrayMove from 'array-move';
+import axios from 'axios';
 import TodoCreateForm from './TodoCreateForm';
 import { SortableContainer } from 'react-sortable-hoc';
 import SortableTodo from './SortableTodo';
@@ -30,16 +30,70 @@ const SortableList = SortableContainer(
   }
 );
 
-export default function TodoList({ url }) {
-  const {
-    list,
-    create,
-    update,
-    destroy,
-    toggleDone,
-    onSortEnd,
-    fetchAndUpdateList,
-  } = useTodos(initialList);
+export default function TodoList() {
+  const [list, setList] = useState([]);
+
+  const url = 'https://sleepy-taiga-81385.herokuapp.com/todo';
+
+  const fetchAndUpdateList = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        const newList = response.data.map(({ _id, name, done }) => ({
+          id: _id,
+          title: name,
+          done,
+        }));
+        setList(newList);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const create = (newTitle) => {
+    axios
+      .post(url, {
+        name: newTitle,
+      })
+      .then((response) => {
+        console.log(response.data);
+        fetchAndUpdateList();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const update = (id, newTitle) => {
+    const updatedList = list.map((todo) =>
+      todo.id === id ? { ...todo, title: newTitle } : todo
+    );
+    setList(updatedList);
+  };
+
+  const destroy = (id) => {
+    axios
+      .delete(`${url}/${id}`)
+      .then((response) => {
+        console.log(response.data);
+        fetchAndUpdateList();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const toggleDone = (id) => {
+    const currentDoneStatus = list.find((todo) => todo.id === id).done;
+    axios
+      .put(`${url}/${id}`, {
+        done: !currentDoneStatus,
+      })
+      .then((response) => {
+        console.log(response.data);
+        fetchAndUpdateList();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setList((items) => arrayMove(items, oldIndex, newIndex));
+  };
 
   const shouldCancelStart = (e) => {
     const nodeNames = ['svg', 'path', 'BUTTON', 'INPUT'];
@@ -47,8 +101,9 @@ export default function TodoList({ url }) {
   };
 
   React.useEffect(() => {
+    console.log('IN USE EFFECT');
     fetchAndUpdateList();
-  }, [fetchAndUpdateList]);
+  }, []);
 
   return (
     <div className="TodoList">
